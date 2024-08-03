4e716363823133984db62ac47297ef1ded8b7173
jessicabr16 <133373725+jessicabr16@users.noreply.github.com>
2024-08-03 22:02:32 +0000

    atualizando

--- app.py
+++ app.py
@@ -5,3 +5,35 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/sobrenos')
+def sobrenos():
+    return render_template('sobrenos.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/doacoes')
+def doacoes():
+    return render_template('doacoes.html')
+
+@app.route('/campanhas')
+def campanhas():
+    return render_template('campanhas.html')
+
+@app.route('/doar')
+def doar():
+    return render_template('doar.html')
+
+@app.route('/cadastrarongs')
+def cadastrarongs():
+    return render_template('cadastrarongs.html')
+
+
+if __name__ == __main__:
+    app.run()
